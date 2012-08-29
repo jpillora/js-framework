324ec77c29b36a1b36b9953f59fe815055741d43
Jaime Pillora <jaime@gmail.com>
2012-08-29 17:42:31 +1000

    init

---
+++ js/util/backbone-extensions.js
@@ -0,0 +1,43 @@
+define(['backbone'], function() {
+
+  //throw away logs on older browsers
+  if(window.console === undefined)
+      window.console = { log: function(str){} }
+  
+  var logFn = function(str) { 
+    console.log(
+      (this.name||this.cid) + ": " + 
+      (this.model&&this.model.id?(this.model.id+": "):"") + 
+      str); 
+  };
+
+  //extra model methods
+  _.extend(Backbone.Model.prototype, {
+    log: logFn
+  });
+  //extra collection methods
+  _.extend(Backbone.Collection.prototype, {
+    log: logFn
+  });
+  //extra view methods
+  _.extend(Backbone.View.prototype, {
+
+    log: logFn,
+    renderUJS: function() {
+      var view = this;
+      view.setupAutosave(view.model);
+    },
+    //custom editable field initialiser
+    setupAutosave: function(model) {
+      function saveValue() {
+        var n = $(this).attr('data-name');
+        var v = $(this).data('val') || $(this).val();
+        view.model.set(n,v);
+        log("save " + n + "   " + v)
+      }
+      view.$("input[type=text][data-name]").on('save',saveValue);
+      view.$("input[type=checkbox][data-name]").click(saveValue);
+    }
+  });
+  
+});
---
+++ js/util/jquery-extensions.js
@@ -0,0 +1,49 @@
+define(['jquery'], function() {
+
+  //extra jquery methods
+  $.fn.flash = function(color) {
+    var tmp = $(this).css('background-color');
+    $(this)
+      .animate({'background-color':color}, '100')
+      .animate({'background-color':tmp}, '50');
+  };
+
+  $.fn.filterByName = function(name) {
+    return this.filter(function(i,e) {
+      return $(e).nameContains(name); 
+    });
+  }
+
+  $.fn.nameContains = function(name) {
+    return this.attr("name") && this.attr("name").match("[^\\w]"+name+"[^\\w]"); 
+  }
+
+  $.fn.equals = function(that) {
+    if($(this).length !== that.length)
+      return false;
+    for(var i=0,l=$(this).length;i<l;++i)
+      if($(this)[i] !== that[i])
+        return false;
+    return true;
+  }
+
+  $.titlise = function(str) {
+    var splitChar = str.indexOf(' ') > 0 ? ' ' : '_';
+    var parts = str.split(splitChar);
+    for(var p = 0; p < parts.length; ++p)
+      parts[p] = parts[p].charAt(0).toUpperCase() + parts[p].substr(1).toLowerCase();
+    return parts.join(' ');
+  }
+
+  $.scrollTo = function(y) {
+    $('html, body').animate({
+      scrollTop: y
+    }, 100);
+  }
+
+  $.fn.scrollTo = function(e) {
+    if(this && this.length > 0)
+      $.scrollTo(0 ? this.is(':hidden') : e.offset().top-100);
+  }
+
+});
