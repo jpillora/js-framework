949f32cd3d5fbe380aed3bbaa5f7baeceb79027d
Jaime Pillora <jpillora@gmail.com>
2012-10-08 16:10:30 +1100

    ga fallback

--- js/util/ga.js
+++ js/util/ga.js
@@ -1,11 +1,16 @@
 define(['https://ssl.google-analytics.com/ga.js'], function() {
-
-  if(window._gaq === undefined) return null;
-
-  _gaq.push(['_setAccount', 'UA-34352911-1']);
-  _gaq.push(['_trackPageview']);
+  
+  var hasSetup = false;
+  function setup() {
+    if(hasSetup) return true;
+    if(window._gaq === undefined) return false;
+    _gaq.push(['_setAccount', 'UA-34352911-1']);
+    _gaq.push(['_trackPageview']);
+    hasSetup = true;
+  }
 
   function event(category, action, label, value) {
+    if(!setup()) return false;
     var e = ['_trackEvent', category, action];
     if(label) e.push(label);
     if(value) e.push(value);
@@ -13,7 +18,6 @@ define(['https://ssl.google-analytics.com/ga.js'], function() {
   }
 
   return {
-    _gaq: _gaq,
     event: event
   }
   
