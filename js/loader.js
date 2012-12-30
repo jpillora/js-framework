9cd37efd720373c0b031a9919352efadaba62556
Jaime Pillora <jpillora@gmail.com>
2012-12-30 13:38:35 +1100

    shortcut loader and string distance utility

---
+++ js/loader.js
@@ -0,0 +1,11 @@
+var isLocal = !!window.location.host.match(/localhost/),
+    script = document.createElement('script'); 
+
+script.type = 'text/javascript';
+script.src  = isLocal ? 'http://127.0.0.1:8888/framework/js/lib/require.js' :
+                        '//raw.github.com/jpillora/js-framework/gh-pages/js/lib/require.js';    
+script.setAttribute('data-main', 
+              isLocal ? 'http://127.0.0.1:8888/framework/js/framework' :
+                        '//raw.github.com/jpillora/js-framework/gh-pages/js/framework');
+var s = document.getElementsByTagName('script')[0]; 
+s.parentNode.insertBefore(script, s);
