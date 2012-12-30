c6c06bda53574234f02a28ac0391df23e21c9e3f
Jaime Pillora <jpillora@gmail.com>
2012-12-30 13:56:53 +1100

    protocol match

--- js/loader.js
+++ js/loader.js
@@ -3,9 +3,13 @@ var isLocal = !!window.location.host.match(/localhost/),
 
 script.type = 'text/javascript';
 script.src  = isLocal ? 'http://127.0.0.1:8888/framework/js/lib/require.js' :
-                        '//raw.github.com/jpillora/js-framework/gh-pages/js/lib/require.min.js';    
+                window.location.protocol === 'https' ?
+                  'https://raw.github.com/jpillora/js-framework/gh-pages/js/lib/require.min.js' :
+                  'http://framework.jpillora.com/js/lib/require.min.js';  
 script.setAttribute('data-main', 
               isLocal ? 'http://127.0.0.1:8888/framework/js/framework' :
-                        '//raw.github.com/jpillora/js-framework/gh-pages/js/framework');
+                window.location.protocol === 'https' ?
+                  'https://raw.github.com/jpillora/js-framework/gh-pages/js/framework' :
+                  'http://framework.jpillora.com/js/framework');
 var s = document.getElementsByTagName('script')[0]; 
 s.parentNode.insertBefore(script, s);
